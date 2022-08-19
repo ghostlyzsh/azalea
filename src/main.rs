252e435b0fbe6016b6cc01adcc915214fda82a14
ghostlyzsh <pokeball.181818@gmail.com>
2022-08-19 00:14:44 -0400

    TCP server created, nothing being handled yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+
+use std::io;
+
+#[tokio::main]
+async fn main() {
+    let listener = match TcpListener::bind(format!("127.0.0.1:{}", 25565)).await {
+        Ok(n) => n,
+        Err(e) => {
+            panic!("Could not bind to port {}", 25565);
+        }
+    };
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
+        println!("Client connected");
+
+        tokio::spawn(async move {
+
+        });
+    }
+
 }
